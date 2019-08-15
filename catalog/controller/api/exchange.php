b305b61fe1055db0bba95a959241c650d6f97264
Alexander <alexpunga98@mail.ru>
2019-08-15 15:07:21 +0300

    CSS Fix + 100% 1C Exchange

--- catalog/controller/api/exchange.php
+++ catalog/controller/api/exchange.php
@@ -1,107 +1,14 @@
 <?php
-
 class ControllerApiExchange extends Controller {
-	public function index() {
-		$this->load->language('api/exchange');
+	protected $url = '';
+	protected $login = '';
+	protected $password = '';
 
-		$json = array();
-		if($this->config->get('module_exchange_status')) {
-			if(!isset($this->session->data['api_id'])) {
-				$json['error'] = $this->language->get('error_permission');
-				echo "You have no rights to upload product!";
-			} else {
-				if(isset($this->request->post['json'])) {
-					$this->load->model('api/exchange');
+	public function getModuleConfig() {
+		$module_config['url'] = $this->config->get('module_exchange_url');
+		$module_config['login'] = $this->config->get('module_exchange_login');
+		$module_config['password'] = $this->config->get('module_exchange_password');
 
-					switch($this->request->post['action']) {
-						case 'upload':
-							$this->upload($this->request->post['json']);
-							break;
-						case 'modify':
-							$this->modify($this->request->post['json']);
-							break;
-						case 'orders':
-							$this->getOrders();
-							break;
-					}
-				}
-			}
-		} else {
-			echo 'Offline';
-		}
-	}
-
-	public function upload($json) {
-		$json_post = htmlspecialchars_decode($json);
-
-		if(isset($json_post)) {
-			$decoded_json = json_decode($json_post, true);
-		}
-
-		if(isset($decoded_json)) {
-			foreach($decoded_json as $index => $d_json) {
-				$product_data[$index]['model'] = $d_json['model'];
-				$product_data[$index]['sku'] = $d_json['sku'];
-				foreach($d_json['quantity'] as $q => $product_quantity) {
-					$product_data[$index]['quantity'][$q] = $product_quantity;
-				}
-				$product_data[$index]['product_description'][1]['name'] = $d_json['name'];
-				$product_data[$index]['product_description'][2]['name'] = $d_json['name'];
-			}
-
-			if(isset($product_data)) {
-				foreach($product_data as $index => $p_data) {
-					$result[$index] = $this->model_api_exchange->addProduct($p_data);
-				}
-				if(isset($result)) {
-					$errors = 0;
-					$success = 0;
-					foreach($result as $r) {
-						$r ? $success++ : $errors++;
-					}
-					if($success > 0) {
-						if($errors > 0) {
-							echo "Added " . $success . " products of " . count($result) . ". " . $errors . " already exist";
-						} else echo "All products added successful";
-					} else echo "All products already exist";
-				}
-			}
-		}
-	}
-
-	public function modify($json) {
-		$json_post = htmlspecialchars_decode($json);
-
-		if(isset($json_post)) {
-			$decoded_json = json_decode($json_post, true);
-		}
-
-		if(isset($decoded_json)) {
-			foreach($decoded_json as $index => $d_json) {
-				$product_data[$index]['model'] = $d_json['model'];
-				$product_data[$index]['sku'] = $d_json['sku'];
-				$product_data[$index]['quantity'] = $d_json['quantity'];
-				$product_data[$index]['product_description'][1]['name'] = $d_json['name'];
-				$product_data[$index]['product_description'][2]['name'] = $d_json['name'];
-			}
-		}
-		if(isset($product_data)) {
-			foreach($product_data as $index => $p_data) {
-				$result[$index] = $this->model_api_exchange->editProduct($p_data);
-			}
-
-			if(isset($result)) {
-				$errors = 0;
-				$success = 0;
-				foreach($result as $r) {
-					$r ? $success++ : $errors++;
-				}
-				if($success > 0) {
-					if($errors > 0) {
-						echo "Modified " . $success . " products of " . count($result) . ". " . $errors . " not modified";
-					} else echo "All products modified successful";
-				} else echo "No products modified";
-			}
-		}
+		$this->response->setOutput(json_encode($module_config));
 	}
 }
--- catalog/model/api/exchange.php
+++ catalog/model/api/exchange.php
@@ -1,54 +1,6 @@
 <?php
 
 class ModelApiExchange extends Model {
-	public function addProduct($data) {
-		$sku_check = $this->db->query("SELECT sku from " . DB_PREFIX . "product where sku=" . $this->db->escape($data['sku']));
-
-		if(empty($sku_check->row)) {
-			$q = 0;
-			foreach($data['quantity'] as $quantity) {
-				$q += $quantity;
-			}
-
-			$this->db->query("INSERT INTO " . DB_PREFIX . "product SET model = '" . $this->db->escape($data['model']) . "', sku = '" . $this->db->escape($data['sku']) . "', quantity = '" . $q . "', date_added = NOW(), date_modified = NOW()");
-
-			$product_id = $this->db->getLastId();
-
-			foreach($data['product_description'] as $language_id => $value) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "product_description SET product_id = '" . (int)$product_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
-			}
-
-			$locations = $this->db->query("SELECT location_id FROM " . DB_PREFIX . "location");
-
-			foreach($data['quantity'] as $index => $quantity) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "product_to_location SET quantity = " . $quantity . ", product_id = " . (int)$product_id . ", location_id = " . $locations->rows[$index - 1]['location_id']);
-			}
-
-			$this->cache->delete('product');
-
-			return $product_id;
-		} else return false;
-	}
-
-	public function editProduct($data) {
-		$sku_check = $this->db->query("SELECT product_id,sku from " . DB_PREFIX . "product where sku=" . $this->db->escape($data['sku']));
-
-		if(!empty($sku_check->row)) {
-			$q = 0;
-			foreach($data['quantity'] as $quantity) {
-				$q += $quantity;
-			}
-
-			$this->db->query("UPDATE " . DB_PREFIX . "product SET model = '" . $this->db->escape($data['model']) . "', quantity = " . $q . " WHERE sku = " . $this->db->escape($data['sku']));
-
-			foreach($data['quantity'] as $index => $quantity) {
-				$this->db->query("UPDATE " . DB_PREFIX . "product_to_location SET quantity = " . $quantity . " WHERE product_id = " . $sku_check->row['product_id']);
-			}
-
-			return true;
-		} else return false;
-	}
-
 	public function getOrder($order_id) {
 		$sql = "SELECT firstname, lastname, telephone, email, CONCAT( shipping_address_1, ',', shipping_address_2, ' ', shipping_city ) AS delivery_address, payment_method, total FROM " . DB_PREFIX . "order WHERE order_id = " . $order_id;
 
