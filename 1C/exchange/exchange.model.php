a36a5f1fa3d3b91cc52debc2ea59fcdaa5cd9018
vasea <vasile.costiuc@it-lab.md>
2020-04-02 19:43:43 +0300

    1C 2 levels imfort, and update products by model

--- 1C/exchange/exchange.model.php
+++ 1C/exchange/exchange.model.php
@@ -128,6 +128,13 @@ class ModelExchange
             "SELECT count(sku) AS count from " . DB_PREFIX . "product where sku='" . $this->escape($data['sku']) . "'"
         );
 
+		if (!isset($sku_check['row']['count']) || $sku_check['row']['count']==0)
+		{
+			$sku_check = $this->query(
+				"SELECT count(sku) AS count from " . DB_PREFIX . "product where model='" . $this->escape($data['model']) . "'"
+			);
+		}
+
         if (isset($sku_check['row']['count']) && $sku_check['row']['count']) {
             $this->editProduct($data);
         } else {
@@ -220,11 +227,13 @@ class ModelExchange
         return $this->connection->insert_id;
     }
 
+    private $edited_by_model_count=0;
     /**
      * Edit existing product by known data
      * @param array $data
      * @throws Exception
      */
+
     public function editProduct($data)
     {
     	//if($data['sku']=='BXP32220S'){ var_dump('SKKKKKKKKKKKKKKKKKKKKKKKKKKKUUUUUUUUUUUUU BXP32220S');}
@@ -237,20 +246,46 @@ class ModelExchange
         if(isset($category['category_id']) && strlen($category['category_id'])) {
 			$category_id = $category['category_id'];
 			$parent_id = $category['parent_id'];
-			$product_id = $this->query(
+			$result = $this->query(
 				"SELECT product_id from " . DB_PREFIX . "product where sku='" . $this->escape($data['sku']) . "'"
-			)['row']['product_id'];
+			);
+			$update_by_model=false;
+			if(isset($result['row']['product_id'])) {
+				 $product_id = $result['row']['product_id'];
+			}else{
+				if(strlen($this->escape($data['model']))) {
+					$product_id = $this->query(
+						"SELECT product_id from " . DB_PREFIX . "product where model='" . $this->escape($data['model']) . "'"
+					)['row']['product_id'];
+
+					$update_by_model = true;
+				}
+			}
 
 			$status = in_array($product_id,$this->active_products_ids)?1:0;
-
-			$this->query(
+			if($update_by_model){
+				if($status) {
+					$this->query(
+						"UPDATE " . DB_PREFIX . "product SET model = '" . $this->escape(
+							$data['model']
+						) . "', sku = '" . $this->escape(
+							$data['sku']
+						) . "', quantity = " . $data['quantity'] . ", price = " . $data['price'] . ", status = " . $status . " WHERE model = '" . $this->escape(
+							$data['model']
+						) . "'"
+					);
+					$this->edited_by_model_count++;
+					var_dump('UPDATE BY MODEL : ' . $product_id . ' | ' . $this->escape($data['model']) . ' | ' . $product_id . " | $this->edited_by_model_count | {$this->connection->affected_rows} ");
+				}
+			}else {
+				$this->query(
 					"UPDATE " . DB_PREFIX . "product SET model = '" . $this->escape(
 						$data['model']
-					) . "', quantity = " . $data['quantity'] . ", price = " . $data['price'] .", status = " . $status . " WHERE sku = '" . $this->escape(
+					) . "', quantity = " . $data['quantity'] . ", price = " . $data['price'] . ", status = " . $status . " WHERE sku = '" . $this->escape(
 						$data['sku']
 					) . "'"
 				);
-
+			}
 			$main_category = 1;
 			$this->query(
 			"INSERT IGNORE INTO oc_product_to_category SET product_id = $product_id, category_id = $category_id, main_category = $main_category"
@@ -280,7 +315,7 @@ class ModelExchange
         )['row']['count_categories'];
     }
 
-    public function addCategory($data)
+    public function addCategory($data,$depth)
     {
         $parent_id = strlen($data['parent_id']) ? $data['parent_id'] : 0;
 
@@ -288,9 +323,13 @@ class ModelExchange
                 "SELECT category_id FROM oc_category WHERE category_1c = '$parent_id' and information = 0"
             )['row']['category_id'] ?? 0;
 
+        $category_status=$depth>=2?0:1;
         $this->query(
-            "INSERT INTO oc_category SET status= 1,top=0,`column`=1,information=0, category_1c = '{$data['category_id']}', parent_id = $parent_category"
+            "INSERT INTO oc_category SET status=$category_status,top=0,`column`=1,information=0, category_1c = '{$data['category_id']}', parent_id = $parent_category"
         );
+        var_dump(
+        	"INSERT INTO oc_category SET status=$category_status,top=0,`column`=1,information=0, category_1c = '{$data['category_id']}', parent_id = $parent_category"
+		);
         $category_id = $this->getLastId();
         $this->query(
             "INSERT INTO oc_category_description SET category_id = $category_id, language_id = 1, name = '{$data['name']}', meta_title = '{$data['name']}'"
@@ -327,30 +366,51 @@ class ModelExchange
         return $category_id;
     }
 
-    public function updateCategory($data)
+    public function updateCategory($data,$depth)
     {
     	$oc_category=$this->query(
-			"SELECT category_id FROM oc_category WHERE category_1c = '{$data['category_id']}'")['row'];
-        if ( isset($oc_category['category_id']) && isset($data['category_id']) and $data['category_id']) {
-            $this->query(
+			"SELECT category_id,parent_id,status FROM oc_category WHERE category_1c = '{$data['category_id']}'")['row'];
+
+		if ( isset($oc_category['category_id']) && isset($data['category_id']) and $data['category_id']) {
+			$category_status=$depth>=2?0:1;
+
+			$parent_id = strlen($data['parent_id']) ? $data['parent_id'] : 0;
+			$parent_category = $this->query(
+					"SELECT category_id FROM oc_category WHERE category_1c = '$parent_id' and information = 0"
+				)['row']['category_id'] ?? 0;
+			$this->query(
+				"UPDATE oc_category SET status = $category_status, parent_id=$parent_category WHERE category_id = {$oc_category['category_id']}"
+			);
+
+			var_dump(
+				"UPDATE oc_category SET status = $category_status, parent_id=$parent_category WHERE category_id = {$oc_category['category_id']}"
+			);
+			//var_dump("UPDATE oc_category SET status = $category_status WHERE category_id = {$oc_category['category_id']}");
+
+			$this->query(
                 "UPDATE oc_category_description SET name = '{$data['name']}' WHERE category_id = {$oc_category['category_id']} and language_id = 1"
             );
         } else {
-            $this->addCategory($data);
+            $this->addCategory($data,$depth);
         }
     }
 
     public function dropOldCategories()
     {
-        $categories = $this->query(
-            "SELECT category_id FROM oc_category WHERE category_1c is null and information = 0"
+        $categories_low_level_to_delete = $this->query(
+            "SELECT category_id FROM oc_category WHERE category_id IN((select category_id FROM oc_category_path GROUP BY category_id Having max(level)<2)) AND (category_1c is null and information = 0)"
         )['rows'];
-        if ($categories) {
-            foreach ($categories as $category) {
-                $implode[] = $category['category_id'];
-            }
-        }
-        if (isset($implode)) {
+        $categories_high_level_to_delete = $this->query(
+			"SELECT category_id FROM oc_category WHERE (NOT (category_1c is null)  and information = 0) AND category_id IN ( SELECT category_id FROM oc_category_path WHERE level>=2)"
+		)['rows'];
+
+        var_dump('categories_low_level_to_delete : ',$categories_low_level_to_delete);
+        var_dump('categories_high_level_to_delete : ',$categories_high_level_to_delete);
+        $implode_low=array_column($categories_low_level_to_delete,'category_id');
+		$implode_high=array_column($categories_high_level_to_delete,'category_id');
+		$implode=array_merge($implode_low,$implode_high);
+
+        if (count($implode)) {
             $this->query("DELETE FROM oc_category_description where category_id in (" . implode(",", $implode) . ")");
             $this->query("DELETE FROM oc_category where category_id in (" . implode(",", $implode) . ")");
             $this->query(
@@ -361,6 +421,30 @@ class ModelExchange
             $this->query("DELETE FROM oc_category_path WHERE category_id in (" . implode(",", $implode) . ")");
         }
     }
+
+    public function dropHighLevelCategories() {
+		$categories = $this->query(
+				 "SELECT category_id FROM oc_category WHERE (NOT (category_1c is null)  and information = 0) AND category_id IN ( SELECT category_id FROM oc_category_path WHERE level>=2)"
+		)['rows'];
+
+		if ($categories) {
+			$high_level_categories_to_delete = array_column($categories,'category_id');
+		}
+
+		if (isset($high_level_categories_to_delete) && count($high_level_categories_to_delete)) {
+			var_dump('high level categories to delete at end :'.count($high_level_categories_to_delete));
+
+			$this->query("DELETE FROM oc_category_description where category_id in (" . implode(",", $high_level_categories_to_delete) . ")");
+            $this->query("DELETE FROM oc_category where category_id in (" . implode(",", $high_level_categories_to_delete) . ")");
+            $this->query(
+				"DELETE FROM oc_product_to_category WHERE category_id in (" . implode(",", $high_level_categories_to_delete) . ")"
+			);
+            $this->query("DELETE FROM oc_category_to_layout WHERE category_id in (" . implode(",", $high_level_categories_to_delete) . ")");
+            $this->query("DELETE FROM oc_category_to_store WHERE category_id in (" . implode(",", $high_level_categories_to_delete) . ")");
+            $this->query("DELETE FROM oc_category_path WHERE category_id in (" . implode(",", $high_level_categories_to_delete) . ")");
+        }
+	}
+
 	public function setProductsStatusInactive() {
     	$this->query("UPDATE oc_product SET status=0");
 	}
