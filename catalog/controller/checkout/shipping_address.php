de34b9664aa87371034e2dbd4a5fc5667f5ae715
Sanchous <alexpunga98@mail.ru>
2020-04-24 17:40:52 +0300

    [Fix]: Payment method choice when user is authorized

--- catalog/controller/checkout/shipping_address.php
+++ catalog/controller/checkout/shipping_address.php
@@ -37,7 +37,7 @@ class ControllerCheckoutShippingAddress extends Controller {
 
 		// Custom Fields
 		$data['custom_fields'] = array();
-		
+
 		$this->load->model('account/custom_field');
 
 		$custom_fields = $this->model_account_custom_field->getCustomFields($this->config->get('config_customer_group_id'));
@@ -53,13 +53,13 @@ class ControllerCheckoutShippingAddress extends Controller {
 		} else {
 			$data['shipping_address_custom_field'] = array();
 		}
-		
+
 		$this->response->setOutput($this->load->view('checkout/shipping_address', $data));
 	}
 
 	public function save() {
 		$this->load->language('checkout/checkout');
-		
+
 		$json = array();
 
 		// Validate if customer is logged in.
@@ -98,7 +98,15 @@ class ControllerCheckoutShippingAddress extends Controller {
 
 		if (!$json) {
 			$this->load->model('account/address');
-			
+
+			if(!isset($this->request->post['shipping_address']) && isset($this->request->post['payment_address'])){
+                $this->request->post['shipping_address'] = $this->request->post['payment_address'];
+
+                if (!isset($this->session->data['payment_address'])) {
+                    $this->session->data['payment_address'] = $this->model_account_address->getAddress($this->request->post['address_id']);
+                }
+            }
+
 			if (isset($this->request->post['shipping_address']) && $this->request->post['shipping_address'] == 'existing') {
 				if (empty($this->request->post['address_id'])) {
 					$json['error']['warning'] = $this->language->get('error_address');
@@ -168,10 +176,10 @@ class ControllerCheckoutShippingAddress extends Controller {
 					// If no default address ID set we use the last address
 					if (!$this->customer->getAddressId()) {
 						$this->load->model('account/customer');
-						
+
 						$this->model_account_customer->editAddressId($this->customer->getId(), $address_id);
 					}
-					
+
 					unset($this->session->data['shipping_method']);
 					unset($this->session->data['shipping_methods']);
 				}
