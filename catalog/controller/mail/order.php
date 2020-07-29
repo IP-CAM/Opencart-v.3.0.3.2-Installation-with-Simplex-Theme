366b8e539d4012150d17a6473de8839354c68e8f
Alexandru Punga <alexandru.punga@it-lab.md>
2020-07-29 16:35:43 +0300

    Fix mail

--- catalog/controller/mail/order.php
+++ catalog/controller/mail/order.php
@@ -267,8 +267,8 @@ class ControllerMailOrder extends Controller {
 		$mail->smtp_port = $this->config->get('config_mail_smtp_port');
 		$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 
-		$mail->setTo($this->config->get('config_mail_smtp_username'));
-		$mail->setFrom($from);
+		$mail->setTo($from);
+		$mail->setFrom($this->config->get('config_mail_smtp_username'));
 		$mail->setSender(html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8'));
 		$mail->setSubject(html_entity_decode(sprintf($language->get('text_subject'), $order_info['store_name'], $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
 		$mail->setHtml($this->load->view('mail/order_add', $data));
